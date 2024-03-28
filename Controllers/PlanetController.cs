a1135d159854e6ce6cf7d266a676ac3055a7fdc5
captainMony <monybogoev@gmail.com>
2024-03-28 19:40:35 +0200

    Part 1 Prep for rest of the tables

---
+++ Controllers/PlanetController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SoftUniSpaceConsoleWebApp.Controllers
+{
+    public class PlanetController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
