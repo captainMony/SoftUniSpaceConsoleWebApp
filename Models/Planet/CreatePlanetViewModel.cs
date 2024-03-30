35a9b62dcac0adbf21067cc7b3153136b0cfaf72
NiaVu <165158958+NiaVu@users.noreply.github.com>
2024-03-30 22:31:16 +0200

    Create CreatePlanetViewModel.cs

---
+++ Models/Planet/CreatePlanetViewModel.cs
@@ -0,0 +1,11 @@
+namespace SoftUniSpaceConsoleWebApp.Models
+{
+    public class CreatePlanetViewModel
+    {
+        public string Name { get; set; }
+
+        public int Type { get; set; }
+
+        public string UniverseGroup { get; set; }
+    }
+}
